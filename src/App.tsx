f4a1a2e878e111b51da107927e491ed33648fc1a
Dorota Tomala <doforever@gmail.com>
2022-05-20 03:25:16 +0200

    Modify mui theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,42 @@ import Rate from './features/Rate/Rate';
 import TransactionList from './features/TransactionList/TransactionList';
 import MaxTransaction from './features/MaxTransaction/MaxTransaction';
 import AddTransaction from './features/AddTransaction/AddTransaction';
-import { CssBaseline, StyledEngineProvider, Stack, Container } from '@mui/material';
+import { CssBaseline, StyledEngineProvider, Stack, Container, createTheme, ThemeProvider } from '@mui/material';
+
+declare module '@mui/material/styles' {
+  interface Theme {
+    status: {
+      danger: string;
+    };
+  }
+  interface ThemeOptions {
+    status?: {
+      danger?: string;
+    };
+  }
+}
+
+const theme = createTheme({
+  spacing: 6,
+});
 
 function App() {
   return (
     <div>
       <CssBaseline />
       <StyledEngineProvider injectFirst>
-        <Container sx={{ padding: '24px' }} component="main" maxWidth="lg">
-          <Stack spacing={2} alignItems='stretch'>
-            <Stack direction={{xs:'column', md:'row'}} spacing={2}>
-              <Rate />
-              <MaxTransaction />
+        <ThemeProvider theme={theme}>
+          <Container sx={{ padding: '24px' }} component="main" maxWidth="lg">
+            <Stack spacing={2} alignItems='stretch'>
+              <Stack direction={{xs:'column', md:'row'}} spacing={2}>
+                <Rate />
+                <MaxTransaction />
+              </Stack>
+              <AddTransaction />
+              <TransactionList />
             </Stack>
-            <AddTransaction />
-            <TransactionList />
-          </Stack>
-        </Container>
+          </Container>
+        </ThemeProvider>
       </StyledEngineProvider>
     </div>
   );
